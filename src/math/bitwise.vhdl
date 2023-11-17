e256e2a66af7b2e1d7fe473bbfb4fc5b057343c6
Thijs van Esch <me@thijsvanesch.com>
2023-11-17 14:53:48 +0100

    Changed bitwise function to operate on 24-bit words instead of bits

--- src/math/bitwise.vhdl
+++ src/math/bitwise.vhdl
@@ -4,29 +4,39 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package bitwise_ops is
-	function m_and (a : std_logic; b : std_logic) return std_logic;
-	function m_or  (a : std_logic; b : std_logic) return std_logic;
-	function m_not (a : std_logic) return std_logic;
+
+	function m_and (a, b : std_logic_vector(23 downto 0)) 
+	return std_logic_vector;
+
+	function m_or  (a, b : std_logic_vector(23 downto 0)) 
+	return std_logic_vector;
+
+	function m_not (a : std_logic_vector(23 downto 0)) 
+	return std_logic_vector;
+
 end package bitwise_ops;
 
 package body bitwise_ops is
 
-	pure function m_and (a : std_logic; b : std_logic) return std_logic is
-		variable y : std_logic;
+	pure function m_and (a, b : std_logic_vector(23 downto 0)) 
+	return std_logic_vector is
+		variable y : std_logic_vector(23 downto 0);
 	begin
 		y := a and b;
 		return (y);
 	end;
 
-	pure function m_or (a : std_logic; b : std_logic) return std_logic is
-		variable y : std_logic;
+	pure function m_or (a, b : std_logic_vector(23 downto 0)) 
+	return std_logic_vector is
+		variable y : std_logic_vector(23 downto 0);
 	begin
 		y := a or b;
 		return (y);
 	end;
 
-	pure function m_not (a : std_logic) return std_logic is
-		variable y : std_logic;
+	pure function m_not (a : std_logic_vector(23 downto 0)) 
+	return std_logic_vector is
+		variable y : std_logic_vector(23 downto 0);
 	begin
 		y := not a;
 		return (y);
