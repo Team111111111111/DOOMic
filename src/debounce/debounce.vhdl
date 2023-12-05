da8ebed63614380631acf626de5c75ec01089bb8
Bart Jansen <bartantoon@hotmail.nl>
2023-12-05 10:42:58 +0100

    Setup a new branch screw you Step hen, I am not going to writte this shit again

---
+++ src/debounce/debounce.vhdl
@@ -0,0 +1,60 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity debouncer is
+generic
+(
+	max_count  : integer := 32;
+	wait_count : integer := 64
+);
+port
+(
+	clk : in std_logic;
+	en  : in std_logic;
+
+	debounced : out std_logic;
+	state_o   : out std_logic_vector(1 downto 0)
+);
+end entity; -- debouncer
+
+architecture arch of debouncer is
+
+	type state_type is (idle, active, waiting);
+	signal state : state_type := idle;
+
+	signal count : integer := 0;
+
+begin
+
+	s_update : process (clk)
+	begin
+		if rising_edge(clk) then
+			case (state) is
+				when idle =>
+					debounced <= '0';
+					state_o <= "00";
+					if en = '1' then
+						count <= 0;
+						state <= active;
+					end if;
+				when active =>
+					debounced <= '1';
+					state_o <= "11";
+					count <= count + 1;
+					if count >= max_count then
+						count <= 0;
+						state <= waiting;
+					end if;
+				when waiting => 
+					debounced <= '0';
+					state_o <= "10";
+					count <= count + 1;
+					if count >= wait_count then
+						state <= idle;
+					end if;
+			end case;
+		end if;
+	end process; -- s_update
+
+end architecture; -- arch
---
+++ stuff/README.md
@@ -0,0 +1,13 @@
+
+# DOOM
+
+This project is about making IC that runs DOOM engine and works with FPGA that emulates "chip-set" and takes control over the memory management and stuff.
+
+## Naming conventions
+
+All filenames should only consist of lowercase ascii characters and underscores. Files containing VHDL code should have the `.vhdl` extension rather than the `.vhd` extension.
+
+## Repository policies
+
+Committing binary files is not allowed. Rather, commit the raw `.drawio` files. These only contain text. Images can be uploaded directly to pages on the wiki.
+
