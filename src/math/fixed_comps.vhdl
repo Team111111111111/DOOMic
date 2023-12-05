b9959f811559185094c9309be71b3c97b1cae4b9
Kyrian Rahimatulla <k.rahimatulla@student.tudelft.nl>
2023-12-05 09:42:29 +0100

    three fixed comparators

---
+++ src/math/fixed_comps.vhdl
@@ -0,0 +1,50 @@
+Library IEEE:
+use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+package fixed_comparators is
+generic(n_bits : integer := 24);
+	
+	function m_la_16 (a: std_logic_vector(n_bits - 1 downto 0))
+	return std_logic;
+
+	function m_la_32 (a: std_logic_vector(n_bits - 1 downto 0))
+	return std_logic;
+
+	function m_la_48 (a: std_logic_vector(n_bits - 1 downto 0))
+	return std_logic;
+
+end package fixed_comparators;
+
+package body fixed_comparators is
+generic(n_bits : integer := 24)
+
+	pure function m_la_16 (a: std_logic_vector(n_bits - 1 downto 0))
+	return std_logic is
+		variable y: std_logic := '0';
+	begin
+		if unsigned(a) > 16 then
+			y := '1';
+		end if;
+		return (y);
+	end function;
+
+	pure function m_la_32 (a: std_logic_vector(n_bits - 1 downto 0))
+	return std_logic is
+		variable y: std_logic := '0';
+	begin
+		if unsigned(a) > 32 then
+			y := '1';
+		end if;
+		return (y);
+	end function;
+
+	pure function m_la_48 (a: std_logic_vector(n_bits - 1 downto 0))
+	return std_logic is
+		variable y: std_logic := '0';
+	begin
+		if unsigned(a) > 48 then
+			y := '1';
+		end if;
+		return (y);
+	end function;
