ac710a53a83659383ff9345f119a52c39f8c1ea0
Darrennett <131657692+Darrennett@users.noreply.github.com>
2023-12-28 16:11:02 +0100

    removed sel and clk

--- vhdl/hline_plusminus.vhd
+++ vhdl/hline_plusminus.vhd
@@ -4,11 +4,9 @@ use ieee.numeric_std.all;
 
 entity plus_minus is
 	port(
-	clk	: in std_logic;
 	add_1	: in std_logic_vector(15 downto 0);
 	add_2	: in std_logic_vector(15 downto 0);
 	sub	: in std_logic_vector(15 downto 0);
-	sel	: in std_logic;
 	result	: out std_logic_vector(15 downto 0));
 end entity;
 
@@ -30,7 +28,7 @@ component complement is
 	);
 end component;
 
-signal add_r_sig, inverse_r_sig, sub_r_sig : std_logic_vector(15 downto 0);
+signal add_r_sig, inverse_r_sig: std_logic_vector(15 downto 0);
 
 begin
 	addition: adder port map(
@@ -45,22 +43,7 @@ begin
 	subtraction: adder port map(
 		a => inverse_r_sig,
 		b => add_r_sig,
-		r => sub_r_sig);
-
-process(clk)
-begin
-
-        case sel is
-            when '1' =>
-                result <= sub_r_sig;
-            when '0' =>
-                result <= add_r_sig;
-            when others =>
-                result <= (others => '0');
-        end case;
-end process;
-
-end architecture behavioural;
+		r => result);
 
 end architecture behavioural;
 
