cbd93bda18748200c3b5eb6c8df365d803bb53e8
TomRiej <71431978+TomRiej@users.noreply.github.com>
2024-01-12 14:24:10 +0100

    fixed comparators

--- src/vline/comparator.vhdl
+++ src/vline/comparator.vhdl
@@ -1,33 +1,25 @@
 library IEEE;
 use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
 
-entity m_comparator is
-generic(n_bits	:	integer := 24);
-   port(a   : in  std_logic_vector(n_bits - 1 downto 0);
-        b   : in  std_logic_vector(n_bits - 1 downto 0);
-        ala : out std_logic);
-end m_comparator;
+entity fixed_comp_entity is
+   port(a : in  std_logic_vector(21 downto 0);
+		b : in	std_logic_vector(21 downto 0);
+        o : out std_logic);
+end fixed_comp_entity;
 
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-
-architecture behaviour of m_comparator is
-	function fm_comparator 
-		(signal a, b: std_logic_vector (n_bits - 1 downto 0)) 
-			return std_logic is
-		variable ala: std_logic := '0';
-		variable pala: std_logic_vector (n_bits - 1 downto 0);
-		variable bnla: std_logic := '1';
+architecture behaviour of fixed_comp_entity is
+	function f_comparators
+		(signal a : std_logic_vector(21 downto 0);
+		 signal b : std_logic_vector(21 downto 0))
+		return std_logic is
+			variable o : std_logic := '0';
 	begin
-		for i in a'range loop
-			pala(n_bits - 1 - i) := a(n_bits - 1 - i) and 
-(not b(n_bits - 1 - i)) and bnla;
-			bnla := bnla and (a(n_bits - 1 - i) or 
-(not b(n_bits - 1 - i)));
-			ala := ala or pala(n_bits - 1 - i);
-		end loop;
-		return (ala);
-	end fm_comparator;
+		if unsigned(a) > unsigned(b) then
+			o := '1';
+		end if;
+		return (o);
+	end f_comparators;
 begin
-	ala <= fm_comparator(a, b);
+	o <= f_comparators(a, b);
 end behaviour;
