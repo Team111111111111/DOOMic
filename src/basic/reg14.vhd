d982f099139ffbe462596a5b83afd78c1c1e0bfd
Xses <wiktort52@gmail.com>
2024-01-16 11:36:29 +0100

    Now it has a synchronous process and asynch one and it should work

--- src/basic/reg14.vhd
+++ src/basic/reg14.vhd
@@ -11,16 +11,25 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 architecture behaviour of reg14 is
+
+	signal memory, new_memory : std_logic_vector(13 downto 0);
 begin
 	reg: process(clk)
 	begin 
 		if (rising_edge(clk)) then
-			if reset = '0' then
-				REG_OUT <= REG_IN;
+			if reset = '1' then
+				memory <= "00000000000000";
 			else
-				REG_OUT <= "00000000000000";	
+				memory <= new_memory;	
 			end if;
 		end if;
 	end process;
+
+	process(memory, new_memory)
+	begin
+		new_memory <= REG_IN;
+	end process;
+
+	REG_OUT <= memory;
 end behaviour;
 
